150b13fc9488519877d67307237c8796bfc86a22
aspenyoo <aspen.yoo@nyu.edu>
2016-05-25 17:27:13 -0400

    added softmax

--- model.m
+++ model.m
@@ -14,6 +14,7 @@ nS = length(Sdistribution);
 % parameters
 JbarVec = [25 20 10 5];       % shape parameter of gamma distribution
 tau = 3;        % scale parameter of gamma distribution
+beta = 1;
 
 nCond = length(JbarVec); % number of conditions
 % % plot gamma distribution
@@ -27,29 +28,57 @@ slope = 0.4;
 rewardFn = @(r)maxReward*exp(-slope*r/knobConstant);
 
 nTrials = 100;
+nsamp = 100;
 SVec = Sdistribution(ceil(rand(nCond,nTrials).*nS));
 
 colors = [.6 .2 .6; .7 .7 .1; .8 .3 0; 0 .4 .7]; %aspencolors(nCond,'pastel');
-figure; 
+figure;
 r = nan(nCond,nTrials); Shat = nan(nCond,nTrials);
 rho = nan(1,nCond); pval = nan(1,nCond);
 for icond = 1:nCond;
     
     Jbar = JbarVec(icond);
     
+    % get p(J|Jbar,tau)
+    xx = linspace(0,200,nsamp);
+    yy = gampdf(xx,Jbar/tau,tau);
+    ind1 = find(yy >= 1e-10,1,'first') - 1;
+    ind2 = find(yy >= 1e-10,1,'last') + 1;
+    xx = linspace(xx(ind1),xx(ind2),nsamp);
+    yy = gampdf(xx,Jbar/tau,tau);
+    p_sigma = yy./sum(yy);
+    sigval = sqrt(1./xx);
+    
     % samples
     sigmaVec = sqrt(1./gamrnd(Jbar/tau,tau,1,nTrials));
     X = SVec(icond,:) + randn(1,nTrials).*sigmaVec;
     Shat(icond,:) = X;
     
-    p_SgivenX = @(r,sigma)normcdf(r,0,sigma) - normcdf(-r,0,sigma);
+    p_SgivenX = @(r,sigma) (normcdf(r,0,sigma) - normcdf(-r,0,sigma));
+%     p_SDgivenXsigma = @(r,sigma) 1./(1+exp(-beta.*rewardFn(r).*p_bringInsideDisk(r,sigma)));
+%         p_SgivenX = @(r) sum(p_SDgivenXsigma(r,sigval).*p_sigma);
     for itrial = 1:nTrials;
         sigma = sigmaVec(itrial);
         
-        % max expected reward for this sample
-        func = @(r) -rewardFn(r).*p_SgivenX(r,sigma);
-        x0 = rand;
-        r(icond,itrial) = fminsearch(func,x0);
+        
+        % calculate expected utility across disk size range
+        func = @(r) rewardFn(r).*p_SgivenX(r,sigma);
+        xx = linspace(0,25,1e5);
+        EUpdf = exp(beta.*func(xx));
+        
+        EUpdf = EUpdf./sum(EUpdf); % normalize   
+        EUcdf = cumsum(EUpdf);% make cdf
+        
+        % sample from icdf
+        samp = rand;
+        idx = find(abs(EUcdf-samp) == min(abs(EUcdf-samp)));
+        r(icond,itrial) = xx(idx);
+
+%         % max expected reward for this sample
+%         %         
+%         func = @(r) -p_SDgivenXsigma(r,sigma);
+%         x0 = rand;
+%         r(icond,itrial) = fminsearch(func,x0);
     end
     
     error_sacc = abs(SVec(icond,:) - Shat(icond,:));
@@ -58,7 +87,7 @@ for icond = 1:nCond;
     % calculate correlation
     [rho(icond), pval(icond)] = corr(error_sacc',r(icond,:)');
     
-%     pause;
+    %     pause;
     
 end
 defaultplot
@@ -79,7 +108,7 @@ pval
 % end
 % defaultplot
 % title('disk size distributions')
-% 
+%
 % % saccadic error ("general error")
 % figure; hold on;
 % for icond = 1:nCond;
