de9792eb93a183d2b4ed0c73f6467d61ca5dc387
Aspen H. Yoo <aspen.yoo@nyu.edu>
2016-05-26 22:05:45 -0400

    more reasonable parameters..softmax

--- model.m
+++ model.m
@@ -12,42 +12,47 @@ Sdistribution = [a1 a2 a3 a4];
 nS = length(Sdistribution);
 
 % parameters
-JbarVec = [25 20 10 5];       % shape parameter of gamma distribution
-tau = 3;        % scale parameter of gamma distribution
-beta = 1;
+JbarVec = [5 2 0.8];%1./([2 6 10].^2);       % mean parameter of gamma distribution
+tau = 1;%.001;        % scale parameter of gamma distribution
+beta = 1;%15;
 
 nCond = length(JbarVec); % number of conditions
-% % plot gamma distribution
-% samps = gamrnd(Jbar,tau,10000,1);
-% hist(samps,50)
 
 % reward function
 maxReward = 120;
-knobConstant = 33.5738;
 slope = 0.4;
-rewardFn = @(r)maxReward*exp(-slope*r/knobConstant);
+rewardFn = @(r)maxReward*exp(-slope*r);
 
 nTrials = 100;
 nsamp = 100;
 SVec = Sdistribution(ceil(rand(nCond,nTrials).*nS));
 
-colors = [.6 .2 .6; .7 .7 .1; .8 .3 0; 0 .4 .7]; %aspencolors(nCond,'pastel');
+colors = aspencolors(nCond,'pastel');
 figure;
-r = nan(nCond,nTrials); Shat = nan(nCond,nTrials);
+D = nan(nCond,nTrials); Shat = nan(nCond,nTrials);
 rho = nan(1,nCond); pval = nan(1,nCond);
 for icond = 1:nCond;
     
     Jbar = JbarVec(icond);
     
-    % get p(J|Jbar,tau)
-    xx = linspace(0,200,nsamp);
-    yy = gampdf(xx,Jbar/tau,tau);
-    ind1 = find(yy >= 1e-10,1,'first') - 1;
-    ind2 = find(yy >= 1e-10,1,'last') + 1;
+    % ===== get p(J|Jbar,tau) ======
+    % make range small enough
+    ind1 = []; ind2 = [];
+    blah = 10;
+    while isempty(ind2) || isempty(ind2); % make range smaller if nothing is above 1e-10
+        blah = blah * 0.9;
+        xx = linspace(0,blah,nsamp);
+        yy = gampdf(xx,Jbar/tau,tau);
+        ind1 = find(yy >= 1e-6,1,'first')-1;
+        ind2 = find(yy >= 1e-6,1,'last')+1;
+    end
+    if ind1 == 0; ind1 = 1; end
+    if ind2 == length(yy)+1; ind2 = length(yy); end
     xx = linspace(xx(ind1),xx(ind2),nsamp);
     yy = gampdf(xx,Jbar/tau,tau);
     p_sigma = yy./sum(yy);
     sigval = sqrt(1./xx);
+%     figure; plot(xx,yy); 
     
     % samples
     sigmaVec = sqrt(1./gamrnd(Jbar/tau,tau,1,nTrials));
@@ -63,29 +68,33 @@ for icond = 1:nCond;
         
         % calculate expected utility across disk size range
         func = @(r) rewardFn(r).*p_SgivenX(r,sigma);
-        xx = linspace(0,25,1e5);
-        EUpdf = exp(beta.*func(xx));
+        x = linspace(0,10,1e5);
+        EUpdf = exp(beta.*func(x));
         
         EUpdf = EUpdf./sum(EUpdf); % normalize   
         EUcdf = cumsum(EUpdf);% make cdf
         
+%         figure;
+%         subplot(3,1,1)
+%         plot(x,func(x))
+%         subplot(3,1,2)
+%         plot(x,EUpdf)
+%         subplot(3,1,3)
+%         plot(x,EUcdf)
+        
         % sample from icdf
         samp = rand;
         idx = find(abs(EUcdf-samp) == min(abs(EUcdf-samp)));
-        r(icond,itrial) = xx(idx);
+        D(icond,itrial) = x(idx);
 
-%         % max expected reward for this sample
-%         %         
-%         func = @(r) -p_SDgivenXsigma(r,sigma);
-%         x0 = rand;
-%         r(icond,itrial) = fminsearch(func,x0);
     end
     
+%     figure
     error_sacc = abs(SVec(icond,:) - Shat(icond,:));
-    plot(error_sacc,r(icond,:),'o','Color',colors(icond,:));hold on;
+    plot(error_sacc,D(icond,:),'o','Color',colors(icond,:));hold on;
     
     % calculate correlation
-    [rho(icond), pval(icond)] = corr(error_sacc',r(icond,:)');
+    [rho(icond), pval(icond)] = corr(error_sacc',D(icond,:)');
     
     %     pause;
     
@@ -95,6 +104,7 @@ xlabel('saccadic error');
 ylabel('disk size')
 
 
+ 
 rho
 pval
 
--- plot_expectedreward.m
+++ plot_expectedreward.m
@@ -4,25 +4,17 @@
 
 % reward function
 maxReward = 120;
-knobConstant = 33.5738;
 slope = 0.4;
-rewardFn = @(r)maxReward*exp(-slope*r/knobConstant);
-
-% % plot reward as a function of r
-% r = linspace(0,650,50);
-% plot(r,rewardFn(r))
+rewardFn = @(r)maxReward*exp(-slope*r);
 
 % p(S|X)
 cushion = 15; % degrees next to cardinal axes in which targets are not presented
 p_S = 1/(90-2*cushion); % uniform prior of degree presentation
-% S = rand*(90-2*cushion) + cushion; % sample S
-% X = S + randn*sigma; % sample X
-% p_SgivenX = @(r,sigma)p_S*(normcdf(r,0,sigma) - normcdf(-r,0,sigma)); %p_S*normpdf(X,S,sigma); % posterior
 p_SgivenX = @(r,sigma)normcdf(r,0,sigma) - normcdf(-r,0,sigma);
 
 % max expected reward for this sample
 nSigma = 50;
-sigmaVec = linspace(0,600,nSigma);
+sigmaVec = linspace(0,20,nSigma);
 for isigma = 1:nSigma;
     sigma = sigmaVec(isigma);
     func = @(r) -rewardFn(r).*p_SgivenX(r,sigma);
@@ -33,24 +25,19 @@ end
 % plot optimal radius as a function of noise
 plot(sigmaVec,r,'k-')
 defaultplot
-ylabel('optimal radius (pixels)')
-xlabel('internal memory noise (pixels)')
-% title('ONE DIMENSIONAL CASE!')
-
-% figure; 
-% plot(sigmaVec,-nll);
-% defaultplot;
-% ylabel('expected reward')
-% xlabel('internal memory noise')
-
+ylabel('optimal radius, D* (dva)')
+xlabel('uncertainty')
+ax = gca;
+    ax.XTick = [0 10 20];
+    
 % plot expected utility for given noise as a function of disk size
 figure;
-sigma = 100;
-rVec = 1:500;
+sigma = 6;
+rVec = linspace(0,25,100);
 EU = rewardFn(rVec).*p_SgivenX(rVec,sigma);
 plot(rVec,EU,'k-')
 defaultplot;
-xlabel('disk size')
+xlabel('disk size, D (dva)')
 ylabel('expected utility')
 
 
