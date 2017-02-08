e09e163b2d5b46532949a5bdf92576f796fc4da0
Aspen H. Yoo <aspen.yoo@nyu.edu>
2017-02-08 16:23:33 -0500

    rayleigh distribution

--- calc_p_Hit.m
+++ calc_p_Hit.m
@@ -7,26 +7,7 @@ function [p_Hit] = calc_p_Hit(r,J)
 % ============ OUTPUT VARIABLES ===========
 % P_HIT: an nR x nJ vector of p(Hit) for each combination of r and J. 
 
-r = r(:)'; % check to make sure r is a horizontal vector
-J = J(:); % J is vertical
+r = r(:); % r is vertical
+J = J(:)'; % J is horizontal
 
-edgee = 2;
-nsamps = 100;
-rangee = linspace(-edgee,edgee,nsamps);
-
-[xx,yy] = meshgrid(rangee,rangee);
-xx = xx(:); yy = yy(:);
-
-% make covariance matrix
-nJs = length(J);
-Sigma = zeros(1,2,nJs*nsamps^2);
-Sigma(1,:,:) = sort(repmat(sqrt(1./J),nsamps^2,2),'descend')'; % in descending order to keep J ascending
-
-% p(X|S)
-p_XgivenS = mvnpdf(repmat([xx yy],nJs,1),0,Sigma);
-p_XgivenS = reshape(p_XgivenS,nsamps^2,1,nJs);
-p_XgivenS = exp(bsxfun(@minus,log(p_XgivenS),log(sum(p_XgivenS)))); % normalize
-
-% p(Hit). sum all entries within the circle of radius r
-idxs = bsxfun(@(x,y) x <= y, xx.^2+yy.^2, r.^2);
-p_Hit = permute(sum(bsxfun(@times,p_XgivenS,idxs)),[2 3 1]); 
+p_Hit = bsxfun(@(x,sigma) 1-exp(-(x.^2)./(2*sigma.^2)),r,sqrt(1./J));
