8425e36c5a37a6cf69b4a2ff3d4586cd53dfce0c
Aspen H. Yoo <aspen.yoo@nyu.edu>
2017-02-22 17:53:02 -0500

    debugging cluster

--- fit_parameters.m
+++ fit_parameters.m
@@ -4,7 +4,7 @@ if nargin < 2; nStartVals = 1; end
 load('cleandata.mat')
 subjdata = data{subjnum};
 
-filepath = '/home/ay963/SWM/spatialWM/fits/';
+filepath = '/home/ay963/spatialWM/fits/';
 filename = [filepath 'fits_optimal_subj' num2str(subjnum) '.mat'];
 
 lb = [1e-5 1e-5 1e-5]; % Jbar_total, tau, beta, lapse (ASPEN FIGURE OUT LAPSE STUFF)
@@ -20,7 +20,8 @@ pub(logflag) = log(pub(logflag));
 for istartvals = 1:nStartVals;
     try load(filename); catch; ML_parameters = []; nLLVec = []; end
     startingval = plb + rand(1,3).*(pub - plb);
-    [x,fval] = bps(@(x) calc_nLL(subjdata,x),startingval,lb,ub,plb,pub);
+    objfunc = @(x) calc_nLL(subjdata,x);
+    [x,fval] = bps(objfunc,startingval,lb,ub,plb,pub);
     x(logflag) = exp(x(logflag));
     ML_parameters = [ML_parameters; x];
     nLLVec = [nLLVec fval];
