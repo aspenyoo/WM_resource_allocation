fc176885942ac4c641301d273e29537387789d85
aspenyoo <aspen.yoo@nyu.edu>
2017-07-17 12:48:15 -0400

    changed script

--- model/modelrecov_testmodelopt.s
+++ model/modelrecov_testmodelopt.s
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=06:00:00
 #SBATCH --mem=2GB
-#SBATCH --job-name=SWM_optimal
+#SBATCH --job-name=SWM_modelrecov_testmodelopt
 #SBATCH --mail-type=END
 #SBATCH --mail-user=aspen.yoo@nyu.edu
 #SBATCH --output=modelrecov_testmodelopt_%a.out
@@ -20,13 +19,14 @@ addpath(genpath('/home/ay963/spatialWM'))
 expnumber = 2;
 blah = num2str($SLURM_ARRAY_TASK_ID);
 truemodel = str2double(blah(1));
-subjnum = str2double(blah(2:end-2));
+subjnum = str2double(blah(2));
 runlistidx = str2double(blah(end-1:end));
 
+nSubj = 11;
 testmodel = 1;
 runmax = 50;
 runlist = [runlistidx runlistidx+runmax/2];
 
-fit_parameters(model,subjnum,runlist,runmax,[],expnumber)
+fit_parameters(testmodel,subjnum+nSubj+1,runlist,runmax,truemodel,expnumber)
 
 EOF
