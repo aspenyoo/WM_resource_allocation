0658e39b5e7d6c58e0e4aa730cc7256fca37ee0b
Aspen H. Yoo <aspen.yoo@nyu.edu>
2017-07-27 17:05:28 -0400

    fits

--- model/fit_parameters.m
+++ model/fit_parameters.m
@@ -24,7 +24,7 @@ if nargin < 7; fixparams = []; end
 
 % filepath = ['fits/exp' num2str(expnumber) '_fixedrisk/'];
 if isempty(fixparams)
-% filepath = ['fits/exp' num2str(expnumber) '/'];
+%  filepath = ['fits/exp' num2str(expnumber) '/'];
 filepath = ['/home/ay963/spatialWM/fits/exp' num2str(expnumber) '/'];
 else
 filepath = ['/home/ay963/spatialWM/fits/exp' num2str(expnumber) '_fixedrisk/'];
